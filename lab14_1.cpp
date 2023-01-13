d5eb6fd436e1091c49528d9e0c42c01fdcb40830
pattharajarin <pattharajarin_p@cmu.ac.th>
2023-01-13 16:31:43 +0700

    ok

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,32 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    for (int i=0 ; i<n ;i++){
+        b[0] += a[i];
+        b[1] += pow(a[i],2);
+        b[2] += log10(a[i]);
+        b[3] += 1/a[i];
+    }
+    b[0] = b[0]/n;
+    b[1] = sqrt((b[1]/n)-(pow(b[0],2)));
+    b[2] = pow(10,b[2]/n);
+    b[3]= n/b[3];
+
+    double max = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+            b[4] = max;
+        }
+    }
+
+    double min = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] < min) {
+            min = a[i];
+            b[5] = min;
+        }
+    }
+}
